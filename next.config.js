ab85d527cfa72119fcc2a7f9b780a24d6b9ba758
Evergreentots Daycare <evergreentotsdaycare@gmail.com>
2025-06-25 01:09:50 +0000

    ./node_modules/require-in-the-middle/index.js Critical dependency: requi

--- next.config.js
+++ next.config.js
@@ -17,6 +17,19 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config, { isServer }) => {
+    // These modules are server-side only, so we'll mock them for the client build
+    if (!isServer) {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        '@opentelemetry/exporter-jaeger': false,
+        'require-in-the-middle': false,
+        'handlebars': false,
+      };
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
